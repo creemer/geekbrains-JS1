b952e4f548e3e7976a84580ce45e152a9c73ec1d
Creemer <3888406@gmail.com>
2017-03-11 22:52:00 +0300

    Add files. Make programm in js file.

---
+++ lesson1/app.js
@@ -0,0 +1,13 @@
+var tempC = prompt("Укажите температуру в градусах Цельсия");
+var tempF = (9 / 5) * tempC + 32;
+alert(tempF);
+alert("Далее смотри консоль")
+
+var name = "Vasiliy";
+var admin = name;
+console.log(name);
+
+var a = 1000;
+var b = "108";
+var sum = a + b; 
+console.log(sum);
