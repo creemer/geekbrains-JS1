74152a843e995d89ae518d0bb331f24e06004c11
Alexander Kolpakov <3888406@gmail.com>
2017-03-29 19:19:13 +0300

    Adds two functions to adding pawns to board. And write it on style file

--- lesson5/app.js
+++ lesson5/app.js
@@ -42,3 +42,30 @@ function generateEvenLineSqr() {
         }
     }
 }
+
+function addDarkPawns() {
+    setTimeout("wrapper.getElementsByClassName('lightSqr')[4].classList.toggle('blackPawn')", 300);
+    setTimeout("wrapper.getElementsByClassName('lightSqr')[5].classList.toggle('blackPawn')", 900);
+    setTimeout("wrapper.getElementsByClassName('lightSqr')[6].classList.toggle('blackPawn')", 1500);
+    setTimeout("wrapper.getElementsByClassName('lightSqr')[7].classList.toggle('blackPawn')", 2100);
+
+    setTimeout("wrapper.getElementsByClassName('darkSqr')[4].classList.toggle('blackPawn')", 600);
+    setTimeout("wrapper.getElementsByClassName('darkSqr')[5].classList.toggle('blackPawn')", 1200);
+    setTimeout("wrapper.getElementsByClassName('darkSqr')[6].classList.toggle('blackPawn')", 1800);
+    setTimeout("wrapper.getElementsByClassName('darkSqr')[7].classList.toggle('blackPawn')", 2400);
+
+}
+function addWhitePawns() {
+    setTimeout("wrapper.getElementsByClassName('lightSqr')[24].classList.toggle('whitePawn')", 600);
+    setTimeout("wrapper.getElementsByClassName('lightSqr')[25].classList.toggle('whitePawn')", 1200);
+    setTimeout("wrapper.getElementsByClassName('lightSqr')[26].classList.toggle('whitePawn')", 1800);
+    setTimeout("wrapper.getElementsByClassName('lightSqr')[27].classList.toggle('whitePawn')", 2400);
+
+    setTimeout("wrapper.getElementsByClassName('darkSqr')[24].classList.toggle('whitePawn')", 300);
+    setTimeout("wrapper.getElementsByClassName('darkSqr')[25].classList.toggle('whitePawn')", 900);
+    setTimeout("wrapper.getElementsByClassName('darkSqr')[26].classList.toggle('whitePawn')", 1500);
+    setTimeout("wrapper.getElementsByClassName('darkSqr')[27].classList.toggle('whitePawn')", 2100);
+}
+
+setTimeout('addDarkPawns()', 500);
+setTimeout('addWhitePawns()', 500);
