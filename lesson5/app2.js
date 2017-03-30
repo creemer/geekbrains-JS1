b48ab096ff9d1d8b6e4fa8a809a7c97f8bc74152
Alexander Kolpakov <3888406@gmail.com>
2017-03-30 13:24:40 +0300

    Modify functions: addNumber, addLetters. Modify adding squares to board

--- lesson5/app2.js
+++ lesson5/app2.js
@@ -10,7 +10,7 @@ numbers.classList.add('outSideSqr','flexedColumn');
 wrapper.appendChild(numbers);
 
 for (i = 0; i < 8; i++) {
-    sqr = document.createElement('div');
+    var sqr = document.createElement('div');
     sqr.classList.add('sqrNumbers');
     numbers.appendChild(sqr);
 };
@@ -24,3 +24,40 @@ for (i = 0; i < 8; i++) {
     sqr.classList.add('sqrLetters');
     letters.appendChild(sqr);
 };
+
+function addNumbers() {
+    for (var i = 0; i < 8; i++) {
+        document.getElementsByClassName('sqrNumbers')[i].innerText = (i + 1);
+    }
+}
+
+addNumbers();
+
+function addLetters() {
+    var lettersArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    for (var i = 0; i < 8; i++) {
+        document.getElementsByClassName('sqrLetters')[i].innerText = lettersArr[i];
+    }
+}
+
+addLetters();
+
+for (var i = 0, j = 1; i < 8; i++) {
+    addLineSqr(j);
+    j++;
+}
+
+function addLineSqr(line) {
+    for (var i = 1; i <= 8; i++) {
+        var sqr = document.createElement('div');
+
+        if( (i + line) % 2 == 0) {
+            sqr.classList.add('flexed', 'darkSqr');
+            wrapper.appendChild(sqr);
+        } else {
+            sqr.classList.add('flexed', 'lightSqr');
+            wrapper.appendChild(sqr);
+        }
+    }
+}
+
