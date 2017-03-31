4fd63197bbfac71019ca722f9c39dbb0d126603d
Alexander Kolpakov <3888406@gmail.com>
2017-03-31 22:10:16 +0300

    Some moves in code. Moves Functions down.

--- lesson5/app2.js
+++ lesson5/app2.js
@@ -18,21 +18,20 @@ numbers.classList.add('outSideSqr','flexedColumn');
 wrapper.appendChild(numbers);
 
 /**
- * Adding squares for numbers to number wrapper.
- */
-for (i = 0; i < 8; i++) {
-    var sqr = document.createElement('div');
-    sqr.classList.add('sqrNumbers');
-    numbers.appendChild(sqr);
-};
-
-/**
  * Adding wrapper for letter squares.
  * @type {Element}
  */
 var letters = document.createElement('div');
 letters.classList.add('outUpSqr','flexed');
 wrapper.appendChild(letters);
+
+/**
+ * Adds squares on board.
+ */
+for (var i = 0, j = 1; i < 8; i++) {
+    addLineSqr(j);
+    j++;
+};
 
 /**
  * Adding squares to letter wrapper.
@@ -44,53 +43,36 @@ for (i = 0; i < 8; i++) {
 };
 
 /**
- * Adding letters to letter squares.
+ * Adding squares for numbers to number wrapper.
  */
-function addNumbers() {
-    for (var i = 0; i < 8; i++) {
-        document.getElementsByClassName('sqrNumbers')[i].innerText = (i + 1);
-    }
-}
-
-addNumbers();
+for (i = 0; i < 8; i++) {
+    var sqr = document.createElement('div');
+    sqr.classList.add('sqrNumbers');
+    numbers.appendChild(sqr);
+};
 
 /**
- * Adding letters to squares form array
+ * Adding numbers and letters on board
  */
-function addLetters() {
-    var lettersArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-    for (var i = 0; i < 8; i++) {
-        document.getElementsByClassName('sqrLetters')[i].innerText = lettersArr[i];
-    }
-}
-
+addNumbers();
 addLetters();
 
 /**
- * Adds squares on board.
+ * Adding figures on board
  */
-for (var i = 0, j = 1; i < 8; i++) {
-    addLineSqr(j);
-    j++;
-}
+setTimeout('addFigures(figures.pawn.white.position, figures.pawn.white.class, 8)', 300);
+setTimeout('addFigures(figures.pawn.black.position, figures.pawn.black.class, 8)', 600);
+setTimeout('addFigures(figures.castle.white.position, figures.castle.white.class, 2)', 900);
+setTimeout('addFigures(figures.castle.black.position, figures.castle.black.class, 2)', 1200);
+setTimeout('addFigures(figures.horse.white.position, figures.horse.white.class, 2)', 1500);
+setTimeout('addFigures(figures.horse.black.position, figures.horse.black.class, 2)', 1800);
+setTimeout('addFigures(figures.soldier.white.position, figures.soldier.white.class, 2)', 2100);
+setTimeout('addFigures(figures.soldier.black.position, figures.soldier.black.class, 2)', 2400);
+setTimeout('addFigures(figures.queen.white.position, figures.queen.white.class, 1)', 2700);
+setTimeout('addFigures(figures.queen.black.position, figures.queen.black.class, 1)', 3000);
+setTimeout('addFigures(figures.king.white.position, figures.king.white.class, 1)', 3300);
+setTimeout('addFigures(figures.king.black.position, figures.king.black.class, 1)', 3600);
 
-/**
- * Adds lines of squares. Adds by 8 squares to line. Check for black and white sqr.
- * @param line - number of line. needs to check black or white sqr will be added
- */
-function addLineSqr(line) {
-    for (var i = 1; i <= 8; i++) {
-        var sqr = document.createElement('div');
-
-        if( (i + line) % 2 == 0) {
-            sqr.classList.add('flexed', 'darkSqr', 'sqr');
-            wrapper.appendChild(sqr);
-        } else {
-            sqr.classList.add('flexed', 'lightSqr', 'sqr');
-            wrapper.appendChild(sqr);
-        }
-    }
-}
 
 /**
  * Objects for figures.
@@ -164,19 +146,6 @@ var figures = {
     },
 }
 
-setTimeout('addFigures(figures.pawn.white.position, figures.pawn.white.class, 8)', 300);
-setTimeout('addFigures(figures.pawn.black.position, figures.pawn.black.class, 8)', 600);
-setTimeout('addFigures(figures.castle.white.position, figures.castle.white.class, 2)', 900);
-setTimeout('addFigures(figures.castle.black.position, figures.castle.black.class, 2)', 1200);
-setTimeout('addFigures(figures.horse.white.position, figures.horse.white.class, 2)', 1500);
-setTimeout('addFigures(figures.horse.black.position, figures.horse.black.class, 2)', 1800);
-setTimeout('addFigures(figures.soldier.white.position, figures.soldier.white.class, 2)', 2100);
-setTimeout('addFigures(figures.soldier.black.position, figures.soldier.black.class, 2)', 2400);
-setTimeout('addFigures(figures.queen.white.position, figures.queen.white.class, 1)', 2700);
-setTimeout('addFigures(figures.queen.black.position, figures.queen.black.class, 1)', 3000);
-setTimeout('addFigures(figures.king.white.position, figures.king.white.class, 1)', 3300);
-setTimeout('addFigures(figures.king.black.position, figures.king.black.class, 1)', 3600);
-
 /**
  * Universal function for adding figures.
  * @param pos - position on board
@@ -186,5 +155,42 @@ setTimeout('addFigures(figures.king.black.position, figures.king.black.class, 1)
 function addFigures(pos, cls, quantity) {
     for (var i = 0; i < quantity; i++) {
         wrapper.getElementsByClassName('sqr')[ pos[i] ].classList.toggle(cls);
+    }
+}
+
+/**
+ * Adds lines of squares. Adds by 8 squares to line. Check for black and white sqr.
+ * @param line - number of line. needs to check black or white sqr will be added
+ */
+function addLineSqr(line) {
+    for (var i = 1; i <= 8; i++) {
+        var sqr = document.createElement('div');
+
+        if( (i + line) % 2 == 0) {
+            sqr.classList.add('flexed', 'darkSqr', 'sqr');
+            wrapper.appendChild(sqr);
+        } else {
+            sqr.classList.add('flexed', 'lightSqr', 'sqr');
+            wrapper.appendChild(sqr);
+        }
+    }
+}
+
+/**
+ * Adding letters to squares form array
+ */
+function addLetters() {
+    var lettersArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    for (var i = 0; i < 8; i++) {
+        document.getElementsByClassName('sqrLetters')[i].innerText = lettersArr[i];
+    }
+}
+
+/**
+ * Adding letters to letter squares.
+ */
+function addNumbers() {
+    for (var i = 0; i < 8; i++) {
+        document.getElementsByClassName('sqrNumbers')[i].innerText = (i + 1);
     }
 }
